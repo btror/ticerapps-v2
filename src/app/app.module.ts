5371133e4f949d5e0dcd971109f8b413f0a94730
Brandon <ticer1999@gmail.com>
2023-06-10 03:51:49 -0400

    feat: create main components

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationMenuComponent } from './shared/navigation-menu/navigation-menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+import { GraphicComponent } from './shared/graphic/graphic.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavigationMenuComponent,
+    FooterComponent,
+    ContactComponent,
+    ProductsComponent,
+    GraphicComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
