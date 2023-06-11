ae409564c279bc08b493c5aaa86592a98b018b88
Brandon <ticer1999@gmail.com>
2023-06-10 21:41:08 -0400

    feat: nav menu scrolls to sections on home

--- src/app/shared/navigation-menu/navigation-menu.component.ts
+++ src/app/shared/navigation-menu/navigation-menu.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-menu',
   templateUrl: './navigation-menu.component.html',
-  styleUrls: ['./navigation-menu.component.scss']
+  styleUrls: ['./navigation-menu.component.scss'],
 })
 export class NavigationMenuComponent {
+  constructor(private elementRef: ElementRef) {}
 
+  scrollToSection(sectionId: string) {
+    const sectionElement = document.getElementById(sectionId);
+    if (sectionElement) {
+      const yOffset =
+        sectionElement.getBoundingClientRect().top + window.scrollY;
+      window.scrollTo({ top: yOffset, behavior: 'smooth' });
+    }
+  }
 }
