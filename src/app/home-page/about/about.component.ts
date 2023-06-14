c8d539a07926c2d32aa2eae0614b3721e76d6f88
Brandon <ticer1999@gmail.com>
2023-06-13 21:38:59 -0400

    feat: lazy load large images

--- src/app/home-page/about/about.component.ts
+++ src/app/home-page/about/about.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
+  ngOnInit() {
+    this.lazyLoadImage();
+  }
+
+  lazyLoadImage() {
+    const imgElement = this.elementRef.nativeElement.querySelector('img[lazy]');
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const imgSrc = entry.target.getAttribute('src') ?? '';
+          this.renderer.setAttribute(entry.target, 'src', imgSrc);
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    observer.observe(imgElement);
+  }
 }
