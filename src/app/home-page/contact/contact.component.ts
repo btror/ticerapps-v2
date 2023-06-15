00f3337191182cff178e0dfc887b374644d4ad00
Brandon <ticer1999@gmail.com>
2023-06-14 21:03:21 -0400

    feat: send mail from contact section

--- src/app/home-page/contact/contact.component.ts
+++ src/app/home-page/contact/contact.component.ts
@@ -4,37 +4,32 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  onSubmit(form: NgForm, event: Event): void {
-    event.preventDefault(); // Prevent form submission
+  name!: string;
+  email!: string;
+  message!: string;
 
-    // Get form values
+  onSubmit(form: NgForm, event: Event): void {
+    event.preventDefault();
+
     const name = form.value.name;
     const email = form.value.email;
     const message = form.value.message;
-    console.log(name);
-    console.log(email);
-    console.log(message);
 
-    // Compose the email message
     let emailBody = `Name: ${name}\n`;
     emailBody += `Email: ${email}\n`;
     emailBody += `Message: ${message}`;
 
-    // Send the email using a service or API
-    // Replace the following line with your email sending logic
-    this.sendEmail('your-email@example.com', 'Contact Form Submission', emailBody);
+    this.sendEmail('btrorapps@gmail.com', 'Contact Form Submission', emailBody);
 
-    // Reset the form after submission
     form.resetForm();
   }
 
   sendEmail(to: string, subject: string, body: string): void {
-    // Implement your email sending logic here
-    // You can use an email service or API to send the email
-    // For example, you can use the "mailto" URL scheme to open the user's default email client:
-    // window.location.href = `mailto:${to}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.location.href = `mailto:${to}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
   }
 }
